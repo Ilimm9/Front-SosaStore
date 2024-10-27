5ff501e4ccd973a53c029ad99126b65f8e2897a9
Ilian Martínez <ili03maro@gmail.com>
2024-10-27 16:42:42 -0600

    Agregando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdministradorComponent } from './administrador/administrador.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { CajeroComponent } from './cajero/cajero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:LoginComponent},
+    {path:'login',component:LoginComponent},
+    {path:'administrador',component:AdministradorComponent},
+    {path:'inventario',component:InventarioComponent},
+    {path:'cajero',component:CajeroComponent},
+
+
+];
