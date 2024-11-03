e869d03df851f605490059a0ff37f26e95614dff
Pedro Lopez <peterlopezthrs@gmail.com>
2024-11-03 00:18:31 -0600

    version estable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../Servicios/login-service.service';
 
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 
 import { UsuarioLoggedService } from '../Servicios/usuario-logged.service';
 import { Usuario } from '../models/usuario';
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
   errorUsuario: string = '';
   errorPassword: string = '';
   mostrarRecuperacion: boolean = false;
-  emailRecuperacion: string = ''; 
+  emailRecuperacion: string = '';
 
   usuarioLogged: Usuario = new Usuario();
 
@@ -51,59 +51,23 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-   
-       let isValid = true;
-       if (!this.usuario) {
-         this.errorUsuario = 'El campo usuario no puede estar vacío.';
-         isValid = false;
-       }
-       if (!this.password) {
-         this.errorPassword = 'El campo contraseña no puede estar vacío.';
-         isValid = false;
-       }
-   
-       if (!isValid) {
-         return; 
-       }
-   
-       this.authService.login(this.usuario, this.password).subscribe(
-         response => {
-           const rol = response.id_Rol;
-   
-           // Redirigir según el rol del usuario
-           if (rol === 1) {
-             this.router.navigate(['gestor/cajero']);
-           } else if (rol === 2) {
-             this.router.navigate(['gestor/inventario']); 
-           } else if (rol === 3) {
-             this.router.navigate(['gestor/administrador']); 
-           } else {
-             console.log('Verifica tus datos');
-           }
-         },
-         error => {
-           this.errorMessage = 'Usuario o contraseña incorrectos';
-         }
-       );
-     }
 
-  onInputChange() { 
-      this.errorUsuario = ''; 
-      this.errorPassword = ''; 
-      this.errorMessage = '';
-  }  
+    let isValid = true;
+    if (!this.usuario) {
+      this.errorUsuario = 'El campo usuario no puede estar vacío.';
+      isValid = false;
+    }
+    if (!this.password) {
+      this.errorPassword = 'El campo contraseña no puede estar vacío.';
+      isValid = false;
+    }
 
-  mostrarRecuperacionContrasenia() {
-    this.mostrarRecuperacion = true;
-    this.onInputChange();
-  }
+    if (!isValid) {
+      return;
+    }
 
-  recuperarContrasenia() {
-    this.authService.recuperarContrasenia(this.emailRecuperacion).subscribe(
+    this.authService.login(this.usuario, this.password).subscribe(
       response => {
-        console.log('Solicitud de recuperación enviada:', response);
-        this.mostrarRecuperacion = false;
-        this.errorMessage = 'Solicitud de recuperación enviada, revisa tu correo.';
         console.log('Servicio aplicado:', response);
 
         this.usuarioLogged.nombre = response.nombre;
@@ -112,7 +76,7 @@ export class LoginComponent implements OnInit {
 
         // Redirigir según el rol del usuario
 
-        const rol = response.id_Rol; 
+        const rol = response.id_Rol;
 
         if (rol === 1) {
           this.router.navigate(['gestor/cajero']);
@@ -137,8 +101,47 @@ export class LoginComponent implements OnInit {
         } else {
           console.log('Verifica tus datos');
         }
+      },
+      error => {
+        this.errorMessage = 'Usuario o contraseña incorrectos';
+      }
+    );
+  }
 
+  onInputChange() {
+    this.errorUsuario = '';
+    this.errorPassword = '';
+    this.errorMessage = '';
+  }
 
+  mostrarRecuperacionContrasenia() {
+    this.mostrarRecuperacion = true;
+    this.onInputChange();
+  }
+
+  recuperarContrasenia() {
+    this.authService.recuperarContrasenia(this.emailRecuperacion).subscribe(
+      response => {
+        console.log('Solicitud de recuperación enviada:', response);
+        this.mostrarRecuperacion = false;
+        this.errorMessage = 'Solicitud de recuperación enviada, revisa tu correo.';
+        console.log('Servicio aplicado:', response);
+
+        // Redirigir según el rol del usuario
+        const rol = response.id_Rol; 
+
+        if (rol === 1) {
+          this.router.navigate(['gestor/administrador']);
+          console.log("administrador");
+        } else if (rol === 2) {
+          this.router.navigate(['inventario']); 
+          console.log("inventario");
+        } else if (rol == 3){
+          this.router.navigate(['administrador']); 
+          console.log("caja");
+        } else {
+          console.log('Verifica tus datos');
+        }
       },
       error => {
         this.errorMessage = 'Error al intentar recuperar la contraseña';
